bb7fee304d8b1d5a644a1c3ffbaecf39c1fc6759
Remy <remyham2004@gmail.com>
2025-10-03 01:53:40 +0200

    cross modal

--- src/assets/Catalogue.jsx
+++ src/assets/Catalogue.jsx
@@ -26,7 +26,6 @@ export default function Catalogue() {
 
   return (
     <>
-
       {/* HEADER PAGE */}
       <div className="w-full flex flex-col justify-between items-start gap-8 pt-10 pb-20 px-4 sm:px-6 md:px-16 mt-20 bg-white flex-row-900">
         {/* Colonne gauche */}
@@ -71,7 +70,6 @@ export default function Catalogue() {
       {/* GRID PRODUITS */}
       <div className="px-4 sm:px-6 md:px-0">
         {loading ? (
-          // Skeleton loader
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 max-w-[calc(3*522px+2*12px)] mx-auto">
             {Array.from({ length: 6 }).map((_, i) => (
               <div
@@ -112,57 +110,60 @@ export default function Catalogue() {
           className="fixed inset-0 bg-black/90 backdrop-blur-xs flex justify-center items-center z-50 px-4"
           onClick={closeModal}
         >
-          <div
-            className="bg-white/90 w-full max-w-[900px] h-auto max-h-[90vh] relative flex flex-col md:flex-row shadow-lg overflow-y-auto rounded"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div className="md:w-[40%] p-6 md:p-8 bg-white/80 flex flex-col justify-between relative z-10 font-archivo">
-              <div>
-                <h2 className="text-3xl font-bold mb-6 text-[#315747]">
-                  {capitalizeFirst(selectedProduit.Nom)}
-                </h2>
+          <div className="relative w-full max-w-[900px]">
+            {/* Bouton croix */}
+            <button
+              onClick={closeModal}
+              className="absolute top-5 right-5 text-gray-700 hover:text-white text-5xl font-bebas z-20"
+            >
+              ×
+            </button>
 
-                <div className="flex items-center mb-4">
-                  <img src="images/telephone-black.svg" alt="telephone" className="w-5 mr-2" />
-                  <a href="tel:0631498703" className="text-gray-800 hover:underline">
-                    06 31 49 87 03
-                  </a>
+            <div
+              className="bg-white/90 w-full max-h-[90vh] relative flex flex-col md:flex-row shadow-lg overflow-y-auto rounded"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="md:w-[40%] p-6 md:p-8 bg-white/80 flex flex-col justify-between relative z-10 font-archivo">
+                <div>
+                  <h2 className="text-3xl font-bold mb-6 text-[#315747]">
+                    {capitalizeFirst(selectedProduit.Nom)}
+                  </h2>
+
+                  <div className="flex items-center mb-4">
+                    <img src="images/telephone-black.svg" alt="telephone" className="w-5 mr-2" />
+                    <a href="tel:0631498703" className="text-gray-800 hover:underline">
+                      06 31 49 87 03
+                    </a>
+                  </div>
+
+                  <div className="flex items-center mb-6">
+                    <img src="images/email-black.svg" alt="email" className="w-5 mr-2" />
+                    <a href="mailto:quiricipaysage@orange.fr" className="text-gray-800 hover:underline">
+                      quiricipaysage@orange.fr
+                    </a>
+                  </div>
+
+                  <p className="font-archivo text-sm">
+                    Description : {selectedProduit.Description}
+                  </p>
                 </div>
 
-                <div className="flex items-center mb-6">
-                  <img src="images/email-black.svg" alt="email" className="w-5 mr-2" />
-                  <a href="mailto:quiricipaysage@orange.fr" className="text-gray-800 hover:underline">
-                    quiricipaysage@orange.fr
-                  </a>
+                <div
+                  className="bg-yellow-400 text-black rounded-full px-3 py-2 text-lg font-switzer w-max mt-5"
+                  style={{ boxShadow: "7px 10px 10px rgba(250, 204, 21, 0.5)" }}
+                >
+                  {selectedProduit.Prix}
                 </div>
-
-                <p className="font-archivo text-sm">
-                  Description : {selectedProduit.Description}
-                </p>
               </div>
 
-              <div
-                className="bg-yellow-400 text-black rounded-full px-3 py-2 text-lg font-switzer w-max mt-5"
-                style={{ boxShadow: "7px 10px 10px rgba(250, 204, 21, 0.5)" }}
-              >
-                {selectedProduit.Prix}
+              <div className="md:w-[60%] relative overflow-hidden h-[250px] md:h-auto">
+                <img
+                  src={selectedProduit.ImageURL}
+                  alt={`Image du produit ${selectedProduit.Nom}`}
+                  className="w-full h-full object-cover rounded-b md:rounded-l md:rounded-r-none"
+                />
               </div>
             </div>
-
-            <div className="md:w-[60%] relative overflow-hidden h-[250px] md:h-auto">
-              <img
-                src={selectedProduit.ImageURL}
-                alt={`Image du produit ${selectedProduit.Nom}`}
-                className="w-full h-full object-cover rounded-b md:rounded-l md:rounded-r-none"
-              />
-            </div>
-
-          <button
-            onClickCapture={closeModal}
-            className="absolute top-5 right-5 text-gray-700 hover:text-white text-5xl font-bebas"
-          >
-            ×
-          </button>
           </div>
         </div>
       )}
